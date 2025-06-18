4253bf37ec41906dc8dcaee0b6db0ec55ed6c6f1
SonaliRam <sonali.files654@gmail.com>
2025-06-18 17:48:32 +0530

    final changes

--- src/main/java/com/joyful/controller/AdminController.java
+++ src/main/java/com/joyful/controller/AdminController.java
@@ -4,14 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joyful.entity.Admin;
 import com.joyful.service.AdminService;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class AdminController {
 
